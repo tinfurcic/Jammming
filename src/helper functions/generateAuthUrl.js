578772b649f0902f9b466d60b6f5efc39428beab
Tin Furčić <tin.furcic@gmail.com>
2024-05-07 17:39:29 +0200

    Save client id and client secret as environment variables using Netlify.

--- src/helper functions/generateAuthUrl.js
+++ src/helper functions/generateAuthUrl.js
@@ -1,6 +1,6 @@
 import { generateRandomString } from "./generateRandomString";
 
-const client_id = '828454fbd2c14ce993f835d9a85ddc23'; // CHANGE LATER
+const client_id = process.env.REACT_APP_CLIENT_ID; // CHANGE LATER
 const redirect_uri = 'https://tfjammming.netlify.app/callback';
 //const redirect_uri = 'http://localhost:3000/callback'; 
 
