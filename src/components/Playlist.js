f850f6f9a840b7f21c0ef4a80d26ae27bb60f87c
Tin Furčić <tin.furcic@gmail.com>
2024-07-26 17:23:49 +0200

    Confirmation dialogue appears after attempting to delete a playlist.

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Playlist.module.css';
 import getPlaylist from '../helper functions/getPlaylist';
 import noPlaylistImage from '../images/no-playlist-image.png';
@@ -10,14 +10,12 @@ function Playlist ({accessToken, userData, playlistInfo, setPlaylist, setPlaylis
     const isOwned = playlistInfo.owner.id === userData.id;
 
     const openPlaylist = async (playlistInfo) => {
-        if (isOwned) {
-            setIsEditing(true);
-        }
+        console.log(isOwned);
         let playlist = [];
         let nextLink = null;
+        setPlaylist([]);
+        setIsPlaylistLoading(true);
         do {
-            setPlaylist([]);
-            setIsPlaylistLoading(true);
             const data = await getPlaylist(playlistInfo.id, accessToken, nextLink);
             const tracksArray = data.items;
             for (const element of tracksArray) {
@@ -30,17 +28,30 @@ function Playlist ({accessToken, userData, playlistInfo, setPlaylist, setPlaylis
             }
             nextLink = data.next;
         } while (nextLink);
-        setIsPlaylistLoading(false);
         setPlaylist(playlist);
-        setPlaylistName(playlistInfo.name)
+        setIsPlaylistLoading(false);
         setOpenedPlaylistId(playlistInfo.id);
         setIsBrowsing(false);
         setIsManaging(true);
+        if (isOwned) {
+            setPlaylistName(playlistInfo.name)
+            setIsEditing(true);
+        } else {
+            setPlaylistName("(Owned) copy of " + playlistInfo.name);
+            setIsEditing(false);
+        }
     }
+
+    const [areYouSure, setAreYouSure] = useState(false); 
 
     const deletePlaylist = async () => {
         unfollowPlaylist(accessToken, playlistInfo.id);
         setUsersPlaylists(usersPlaylists.filter(playlist => playlist.id !== playlistInfo.id));
+    }
+
+    const handleDelete = () => {
+        deletePlaylist();
+        setAreYouSure(false);
     }
 
     return (
@@ -52,20 +63,34 @@ function Playlist ({accessToken, userData, playlistInfo, setPlaylist, setPlaylis
                 <h3>{playlistInfo.name}</h3>
                 <p>{playlistInfo.description ? decodeHtmlEntities(playlistInfo.description) + " | " : null} {playlistInfo.tracks.total} tracks </p>
             </div>
-            <div className={`${styles.buttonContainer} ${styles.delete} ${isScreenSmall || isScreenSmartphony ? styles.small : ""}`}>
-                <button className={styles.button} onClick={() => deletePlaylist()} >
-                    {isScreenSmall || isScreenSmartphony ? "Delete" : "Delete playlist"}
-                </button>
-            </div>
-            <div className={`${styles.buttonContainer} ${isOwned ? styles.edit : styles.copy} ${isScreenSmall || isScreenSmartphony ? styles.small : ""}`}>
-                <button className={styles.button} onClick={() => openPlaylist(playlistInfo)} >
-                    {isOwned ? (
-                        isScreenSmall || isScreenSmartphony ? "Edit" : "Edit playlist"
-                        ) : (
-                            isScreenSmall || isScreenSmartphony ? "Copy" : "Copy playlist"
-                        )}
-                    </button>
-            </div>
+            {!areYouSure ? (
+                <>
+                    <div className={`${styles.buttonContainer} ${styles.delete} ${isScreenSmall || isScreenSmartphony ? styles.small : ""}`}>
+                        <button className={styles.button} onClick={() => setAreYouSure(true)} >
+                            {isScreenSmall || isScreenSmartphony ? "Delete" : "Delete playlist"}
+                        </button>
+                    </div>
+                    <div className={`${styles.buttonContainer} ${styles.open} ${isScreenSmall || isScreenSmartphony ? styles.small : ""}`}>
+                        <button className={styles.button} onClick={() => openPlaylist(playlistInfo)} >
+                            {isOwned ? (
+                                isScreenSmall || isScreenSmartphony ? "Open" : "Open playlist"
+                                ) : (
+                                    isScreenSmall || isScreenSmartphony ? "Open" : "Open playlist"
+                                )}
+                        </button>
+                    </div>
+                </>
+            ) : (
+                <div className={`${styles.confirmationContainer} ${isScreenSmall || isScreenSmartphony ? styles.small : ""}`}>
+                    <span className={`${isScreenSmall ? styles.wrap : ""}`}>Really delete?</span>
+                    <div className={styles.confirmButtonContainer} >
+                        <button className={`${styles.button} ${styles.no} `} onClick={() => setAreYouSure(false)} >No</button>
+                    </div>
+                    <div className={`${styles.confirmButtonContainer}`} >
+                        <button className={`${styles.button} ${styles.yes} `} onClick={() => handleDelete()} >Yes</button>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
